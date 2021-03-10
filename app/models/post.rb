fe4cc89408104a93f993d2c3f01c340664e43609
megan <megan.schertz@gmail.com>
2021-03-09 18:06:12 -0600

    commit tree cdd08e8e9e56b20c264ae9ff177ee41f9269b96e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+    include ActiveModel::Validations
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, :inclusion => { :in => ['Fiction', 'Non-Fiction'] }
+    validate :clickbait
+
+    def clickbait
+        clickbait_titles = ["Won't Believe","Secret","Top [number]","Guess"]
+        if self.title.nil? || !clickbait_titles.any? {|phrase| self.title.include? phrase}
+            errors.add(:title, "must be clickbait")
+        end
+    end
+
 end
